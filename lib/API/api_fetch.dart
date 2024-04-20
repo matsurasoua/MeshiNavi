b8c4d2c5542f6f3ed8996259e1e8fe08e035b626
松浦 <2210344@ecc.ac.jp>
2024-04-20 16:19:12 +0900

    firstcommit

---
+++ lib/API/api_fetch.dart
@@ -0,0 +1,3 @@
+class Apifech {
+  void printapi() {}
+}
---
+++ lib/setting/settingcolor.dart
@@ -0,0 +1,6 @@
+class Setting_Color {
+  // 背景色
+  static const setting_background = 0xffFFF8F2;
+  // 茶色
+  static const setting_brown = 0xffA33100;
+}
